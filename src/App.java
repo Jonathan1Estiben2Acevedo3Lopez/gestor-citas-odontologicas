c497900be821379399393e24a3063f5f22e9ddf3
Jonathan <jonalopezacevedo@gmail.com>
2026-07-17 19:08:50 -0500

    refactor: renombrar la clase Main a App

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Gestor de citas odontológicas");
+        System.out.println("Entorno Java configurado correctamente");
+    }
+}
