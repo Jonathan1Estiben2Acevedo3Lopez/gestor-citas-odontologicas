229bf97ab1c69b73cdde3b748baf13fdf43298be
Jonathan <jonalopezacevedo@gmail.com>
2026-07-18 23:50:26 -0500

    feat: validar mayoría de edad del paciente

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ public class App {
     public static void main(String[] args) {
 
         String patientName = "Jonathan";
-        int patientAge = 23;
+        int patientAge = 18;
         String patientEmail = "jonathan@example.com";
         boolean hasScheduledAppointment = true;
 
@@ -15,6 +15,12 @@ public class App {
             System.out.println("Tiene cita programada");
         } else {
             System.out.println("No tiene cita programada");
+        }
+
+        if (patientAge >= 18) {
+            System.out.println("El paciente es mayor de edad");
+        } else {
+            System.out.println("El paciente es menor de edad");
         }
 
     }
